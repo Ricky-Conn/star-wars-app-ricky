f7c27c67da5b804c9f918f776bf99fdb7fffd97f
Ricky-Conn <rickysconn@gmail.com>
2021-06-23 20:27:12 +0200

    All components displaying

--- sovtech-rickyconn/src/App.tsx
+++ sovtech-rickyconn/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Page from "./Components/Page"
+import CharacterSummary from "./Components/CharacterSummary"
+import Pagination from "./Components/Pagination"
+import CharacterDetails from "./Components/CharacterDetails"
 
 function App() {
   return (
     <div className="App">
         <h1>App</h1>
+        <Page/>
+        <CharacterSummary/>
+        <Pagination/>
+        <CharacterDetails/>
     </div>
   );
 }
--- sovtech-rickyconn/src/Components/CharacterDetails.tsx
+++ sovtech-rickyconn/src/Components/CharacterDetails.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 
 function CharacterDetails() {
   return (
