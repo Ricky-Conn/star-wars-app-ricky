5a257984c310f43f58d51296733afaf3d945eff2
Ricky-Conn <rickysconn@gmail.com>
2021-06-23 22:50:41 +0200

    Testing different approaches to get CSS-in-JS working

--- sovtech-rickyconn/src/App.tsx
+++ sovtech-rickyconn/src/App.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Page from "./Components/Page"
 import CharacterSummary from "./Components/CharacterSummary"
 import Pagination from "./Components/Pagination"
 import CharacterDetails from "./Components/CharacterDetails"
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { render } from "react-dom";
+import { createUseStyles, ThemeProvider, useTheme } from "react-jss";
 
-function App() {
+useEffect(() =>{
+  const theme = {
+    background: "#f7df1e",
+    color: "#24292e"
+  };
+},[])
+
+const useStyles = createUseStyles({
+  wrapper: {
+    padding: 40,
+    background: ({ theme }) => theme.background,
+    textAlign: "left"
+  },
+  title: {
+    font: {
+      size: 40,
+      weight: 900
+    },
+    color: ({ theme }) => theme.color
+  },
+  link: {
+    color: ({ theme }) => theme.color,
+    "&:hover": {
+      opacity: 0.5
+    }
+  }
+});
+
+const Comp = () => {
+  const theme = useTheme();
+  const classes = useStyles({ theme });
   return (
     <Router>
-      <div className="App">
+      <div>
         <Switch>
           <Route path="/" exact component={Page}/>
           <Route path="/Page" exact component={Page}/>
@@ -20,6 +52,12 @@ function App() {
       </div>
     </Router>
   );
-}
+};
 
-export default App;
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Comp />
+  </ThemeProvider>
+);
+
+render(<App />, document.getElementById("root"));
