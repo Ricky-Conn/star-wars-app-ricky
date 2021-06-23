487ece7e44e0c8b3751718bea861609c337bacda
Ricky-Conn <rickysconn@gmail.com>
2021-06-23 20:28:45 +0200

    Removed default CSS and useless "App" header

--- sovtech-rickyconn/src/App.tsx
+++ sovtech-rickyconn/src/App.tsx
@@ -8,7 +8,6 @@ import CharacterDetails from "./Components/CharacterDetails"
 function App() {
   return (
     <div className="App">
-        <h1>App</h1>
         <Page/>
         <CharacterSummary/>
         <Pagination/>
