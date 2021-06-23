834f31fa26ad731aec096eee5032175a836df517
Ricky-Conn <rickysconn@gmail.com>
2021-06-23 21:41:40 +0200

    Data successfully fetched from Server and stored in variable

--- sovtech-rickyconn/src/Components/Page.tsx
+++ sovtech-rickyconn/src/Components/Page.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Page() {
+
+  const [people, setPeople] = useState<any[]>([])
+
+  useEffect(() =>{
+    fetchPeople();
+  },[])
+
+  const fetchPeople = async () => {
+    const data = await fetch('http://localhost:4000/graphql?query=%7Bpeople%28pageNum%3A1%29%7Bname%7D%7D%0A')
+    const returnedData = await data.json()
+    setPeople(returnedData.data.people)
+  }
+
   return (
     <div>
-      <h1>Star Wars Characters</h1>
+      <h1>Test</h1>
+      {
+        people.map(person => {
+          <h2>{person.name}</h2>
+        })
+      }
     </div>
   );
 }
