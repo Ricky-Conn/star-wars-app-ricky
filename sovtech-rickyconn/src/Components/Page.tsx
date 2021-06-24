e78a9b84b5091b3dcc6bbf17ca396e32e1c8754f
Ricky-Conn <rickysconn@gmail.com>
2021-06-24 19:51:14 +0200

    Removed styles.tsx file in favour of using JavaScript within component

--- sovtech-rickyconn/src/Components/Page.tsx
+++ sovtech-rickyconn/src/Components/Page.tsx
@@ -1,26 +1,43 @@
-import React, {useState, useEffect} from 'react';
-import * as vars from "./styles"
+import React, {useState, useEffect, CSSProperties} from 'react';
 
 
 function Page() {
 
   const [people, setPeople] = useState<any[]>([])
   var i:number = 0;
+  var styles:CSSProperties = {}
 
   useEffect(() =>{
     fetchPeople();
   },[])
 
   const fetchPeople = async () => {
-    document.head.appendChild(
-      document.createElement('style')
-    ).textContent = vars.cardTemplate + vars.personContainer + vars.personSummary + vars.startOpacity
     const data = await fetch('http://localhost:4000/graphql?query=%7Bpeople%28pageNum%3A1%29%7Bname%7D%7D%0A')
     const returnedData = await data.json()
     setPeople(returnedData.data.people)
-    document.head.appendChild(
-      document.createElement('style')
-    ).textContent = vars.endOpacity
+    var summaries = document.getElementsByClassName('summary-card')
+    Array.from(summaries as HTMLCollectionOf<HTMLElement>).forEach(el => {
+      setSummary(el)
+      setTimeout(function()
+      {
+        el.style.opacity = "1"
+      }, 5000);
+    })
+  }
+
+  function setSummary(el)
+  {
+    el.style.margin = "0.5vh"
+    el.style.borderRadius = "0"
+    el.style.width = "100%"
+    el.style.height = "6.2vh"
+    el.style.paddingTop = "10px"
+    el.style.textAlign = "-webkit-center"
+    el.style.color = "white"
+    el.style.verticalAlign = "middle"
+    el.style.background = "linear-gradient(to right, #000000, rgb(160 117 255))"
+    el.style.backgroundRepeat = "no-repeat"
+    el.style.backgroundSize = "cover"
   }
 
   return (
@@ -28,7 +45,7 @@ function Page() {
       {
         people.map(person => {
           i++;
-          return <div style={vars.startOpacity} className={"summary-card"} key={i}>{person.name}</div>
+          return <div style={styles} className="summary-card" key={i}>{person.name}</div>
         })
       }
     </div>
