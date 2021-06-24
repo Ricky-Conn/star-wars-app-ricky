fcc3490cd9c12e6283444559da43e6f12ceb51ce
Ricky-Conn <rickysconn@gmail.com>
2021-06-24 23:14:45 +0200

    Added styling to show responsiveness and clickability and changed formatting

--- sovtech-rickyconn/src/Components/Page.tsx
+++ sovtech-rickyconn/src/Components/Page.tsx
@@ -5,7 +5,9 @@ import store from '../redux/store'
 
 var i:number = 0;
 var styles:CSSProperties = {}
-var people = [{name:"fred"}]
+var people = [{name:null}]
+var defaultHeight = "6.2vh"
+var hoverHeight = "7vh"
 
 const fetchPeople = async () => {
   const data = await fetch('http://localhost:4000/graphql?query=%7Bpeople%28pageNum%3A1%29%7Bname%7D%7D%0A')
@@ -25,7 +27,7 @@ function setSummary(el)
   el.style.margin = "0.5vh"
   el.style.borderRadius = "0"
   el.style.width = "100%"
-  el.style.height = "6.2vh"
+  el.style.height = defaultHeight
   el.style.paddingTop = "10px"
   el.style.textAlign = "-webkit-center"
   el.style.color = "white"
@@ -38,7 +40,13 @@ function setSummary(el)
 
 class Page extends Component{
   mouseEnterStyles=event=>{
-    console.log(event.target)
+    event.target.style.height = hoverHeight
+    event.target.style.boxShadow = "0px 0px 4px 2px rgba(255,255,255,0.49) inset"
+  }
+
+  mouseLeaveStyles=event=>{
+    event.target.style.height = defaultHeight
+    event.target.style.boxShadow = null
   }
 
   render ()
@@ -48,7 +56,14 @@ class Page extends Component{
         {
             store.getState().people.value.map(person => {
               i++;
-              return <div style={styles} className="summary-card" onMouseEnter={this.mouseEnterStyles.bind(this)} key={i}>{person.name}</div>
+              return <div 
+                        style={styles} 
+                        className="summary-card" 
+                        onMouseEnter={this.mouseEnterStyles.bind(this)} 
+                        onMouseLeave={this.mouseLeaveStyles.bind(this)}
+                        key={i}>
+                          {person.name}
+                      </div>
             })
         }
       </div>
