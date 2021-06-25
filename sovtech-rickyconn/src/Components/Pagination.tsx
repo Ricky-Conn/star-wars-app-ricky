2a74aa17add531d730282a8911b68c857cf80a88
Ricky-Conn <rickysconn@gmail.com>
2021-06-25 23:36:58 +0200

    9 elements displaying

--- sovtech-rickyconn/src/Components/Pagination.tsx
+++ sovtech-rickyconn/src/Components/Pagination.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {CSSProperties, Component} from 'react';
+import { connect } from 'react-redux';
+import store from '../redux/store'
 
-function Pagination() {
-  return (
-    <div>
-      <h1>Pagination</h1>
-    </div>
-  );
+var pages: string[] = []
+const numPages: number = 9
+var i:number = 0;
+
+function addPages(numPages:number)
+{
+    for(var i:number = 0; i < numPages; i++)
+    {
+      pages.push('')
+    }
 }
 
-export default Pagination;
+addPages(numPages)
+
+class Pagination extends Component{
+  mouseEnterStyles=event=>{
+  }
+
+  mouseLeaveStyles=event=>{
+  }
+
+  render ()
+  {
+    return(
+      <div id="pageContainer">
+        {
+          pages.map(page =>
+            {
+              i++;
+              return <div className="page-selector" key={i}></div>
+            })
+        }
+      </div>
+    )
+  }
+
+  constructor(props) {
+    super(props);
+  }
+    
+  state = { page: 0}
+}
+
+function mapStateToProps(state) {
+  return { page: state.page.value };
+} 
+
+export default connect(mapStateToProps)(Pagination);
