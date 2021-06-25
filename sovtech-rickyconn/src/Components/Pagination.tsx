cd79ddcee6bbe15418f4e32afb9fe2cbb4ebee91
Ricky-Conn <rickysconn@gmail.com>
2021-06-26 00:30:27 +0200

    Pagination elements showing and updating redux "page" state when clicked

--- sovtech-rickyconn/src/Components/Pagination.tsx
+++ sovtech-rickyconn/src/Components/Pagination.tsx
@@ -1,5 +1,6 @@
 import React, {CSSProperties, Component} from 'react';
 import { connect } from 'react-redux';
+import { setPage } from '../redux/pageSlice';
 import store from '../redux/store'
 
 var pages: string[] = []
@@ -8,10 +9,10 @@ var i:number = 0;
 
 function addPages(numPages:number)
 {
-    for(var i:number = 0; i < numPages; i++)
-    {
-      pages.push('')
-    }
+  for(var i:number = 0; i < numPages; i++)
+  {
+    pages.push('')
+  }
 }
 
 addPages(numPages)
@@ -23,6 +24,10 @@ class Pagination extends Component{
   mouseLeaveStyles=event=>{
   }
 
+  clicked=event=>{
+    store.dispatch(setPage(parseFloat(event.target.innerText)))
+  }
+
   render ()
   {
     return(
@@ -31,7 +36,11 @@ class Pagination extends Component{
           pages.map(page =>
             {
               i++;
-              return <div className="page-selector" key={i}></div>
+              if(i>numPages)
+              {
+                i = 1
+              }
+              return <div className="page-selector" key={i} onClick={this.clicked.bind(this)}>{i}</div>
             })
         }
       </div>
@@ -42,6 +51,25 @@ class Pagination extends Component{
     super(props);
   }
     
+  componentDidMount = () => {
+    const pageContainer = document.getElementById("pageContainer")
+    console.log(pageContainer)
+    if(pageContainer)
+    {
+      pageContainer.style.height = "5vh"
+      pageContainer.style.display = "flex"
+    }
+
+    const pages = document.getElementsByClassName("page-selector")
+    Array.from(pages as HTMLCollectionOf<HTMLElement>).forEach(page => {
+      page.style.background = "blue"
+      page.style.height = "100%"
+      page.style.width = "3vw"
+      page.style.margin = "1px"
+      page.style.color = "white"
+    });
+  }
+
   state = { page: 0}
 }
 
